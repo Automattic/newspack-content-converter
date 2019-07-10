046d61c2428fa10217a444e8c8734152789c8095
Ivan Uravic <iuravic@gmail.com>
2019-07-10 15:55:08 +0200

    First commit

---
+++ newspack-content-converter.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Newspack Content Converter
+ * Description:
+ * Version: 0.0.1-alpha
+ * Author: Automattic
+ * Author URI: https://newspack.blog/
+ * License: GPL2
+ * Text Domain: newspack-content-converter
+ * Domain Path: /languages/
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists('\NewspackContentConverter\Converter') ) {
+	include_once dirname( __FILE__ ) . '/lib/class-converter.php';
+}
+
+( new \NewspackContentConverter\Converter )->init();
---
+++ src/components/src/content-converter/index.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Component } from '@wordpress/element';
+import { __ } from "@wordpress/i18n";
+
+import converter from "./../lib/converter";
+import "./style.css";
+
+class ContentConverter extends Component {
+
+	/**
+	 * Constructor.
+	 */
+	constructor( props ) {
+		super( props  );
+
+		this.state = {
+			isActive: false,
+			postIdsCsv: ""
+		};
+	};
+
+	/*
+	 * Conversion main handler. Chains promises to ensure sequential async operations execution.
+	 */
+	handleOnClick = () => {
+		return Promise.resolve()
+			.then( () => {
+				return new Promise( ( resolve, reject ) => {
+					this.setState( { isActive: true } );
+					resolve();
+				} );
+			} )
+			.then( () => {
+				const { postIdsCsv } = this.state;
+
+				return converter.runMultiplePosts( postIdsCsv );
+			} )
+			.then( () => {
+				return new Promise( ( resolve, reject ) => {
+					this.setState( { isActive: false } );
+					resolve();
+				} );
+			} );
+	};
+
+	/*
+	 * handlePostIdsInputChange().
+	 */
+	handlePostIdsInputChange = ( { currentTarget: input } ) => {
+		const postIdsCsv = input.value;
+
+		this.setState( { postIdsCsv } );
+	};
+
+	/*
+	 * render().
+	 */
+	render() {
+		const { isActive, postIdsCsv } = this.state;
+		const isButtonDisabled = isActive;
+		const labelButton = isActive ? __( "Converting..." ) : __( "Convert" ) ;
+
+		return (
+			<div id="ncc-content">
+				<h1>
+					{ __( "Newspack Content Converter" ) }
+				</h1>
+				<label htmlFor="ncc_post_ids_csv">
+					{ __( "Post IDs CSV" ) }
+				</label>
+				<input
+					type="text"
+					id="ncc_post_ids_csv"
+					placeholder={ __( "Post IDs CSV" ) }
+					value={ postIdsCsv }
+					onChange={ this.handlePostIdsInputChange }
+				>
+				</input>
+				<input
+					type="submit"
+					id="ncc_run_button"
+					value={ labelButton }
+					onClick={ ( event ) => this.handleOnClick( event ) }
+					disabled={ isButtonDisabled }
+				>
+				</input>
+			</div>
+		);
+	}
+
+};
+
+export default ContentConverter;
