d58e49d82e1a22e8fbd78b8d6303ef9edde8ca7e
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-01-15 12:21:40 +0000

    Conversion: Use `newspack-components` to build the page

--- assets/src/conversion/index.js
+++ assets/src/conversion/index.js
@@ -1,8 +1,18 @@
 /**
  * WordPress dependencies.
  */
-import { Component } from '@wordpress/element';
+import { Component, Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+
+/**
+ * Newspack dependencies.
+ */
+import { Button, Card, FormattedHeader, Grid, NewspackLogo, Notice, TextControl } from 'newspack-components';
+
+/**
+ * Material UI dependencies.
+ */
+import UnarchiveIcon from '@material-ui/icons/Unarchive';
 
 /**
  * Internal dependencies.
@@ -104,130 +114,115 @@ class Conversion extends Component {
 
 		if ( '1' == isConversionOngoing ) {
 			return (
-				<div className="ncc-page">
-					<h1>{ __( 'Run Conversion' ) }</h1>
-					<br />
-
-					<h3>{ __( 'A Converson is Currently in Progress' ) }</h3>
-					<p>
-						{ __(
-							'A designated browser tab has already started to convert your content. Conversion progress:'
-						) }
-					</p>
-					<ul>
-						<li>
-							{ __( 'conversion batches processed so far:' ) }{' '}
-							<b>
-								<u>{ queuedBatchesCsv }</u>
-							</b>
-						</li>
-						<li>
-							{ __( 'total batches queued:' ) }{' '}
-							<b>
-								<u>{ maxBatch }</u>
-							</b>
-						</li>
-						<li>
-							{ __( 'conversion batch size (posts per batch):' ) }{' '}
-							<b>
-								<u>{ conversionBatchSize }</u>
-							</b>
-						</li>
-					</ul>
-
-					<br />
-
-					<h3>{ __( 'Reset Ongoing Conversion to Start Again' ) }</h3>
-					<p>
-						{ __(
-							'In case that your active conversion browser tab has been closed by accident, or it has been interrupted and closed unexpectedly, you may reset the conversion status here, and start converting all over again. Note that this will enable you to restart the conversion, but any previous results may be lost.'
-						) }
-					</p>
-					<input
-						type="submit"
-						className="large"
-						value={ __( 'Reset Conversion' ) }
-						onClick={ this.handleOnClickResetConversion }
-					/>
-				</div>
+				<Fragment>
+					<div className="newspack-logo-wrapper">
+						<NewspackLogo />
+					</div>
+					<Grid>
+						<FormattedHeader
+							headerIcon={ <UnarchiveIcon /> }
+							headerText={ __( 'Run conversion' ) }
+							subHeaderText={ __( 'A converson is currently in progress.' ) }
+						/>
+						<Card>
+							<Notice noticeText={ __( 'A designated browser tab has already started to convert your content.' ) } isPrimary />
+							<hr />
+							<h2>{ __( 'Conversion progress' ) }</h2>
+							<TextControl
+								label={ __( 'Conversion batches processed' ) }
+								disabled={ true }
+								value={ queuedBatchesCsv }
+							/>
+							<TextControl
+								label={ __( 'Total conversion batches queued' ) }
+								disabled={ true }
+								value={ maxBatch }
+							/>
+							<TextControl
+								label={ __( 'Conversion batch size (entries per batch)' ) }
+								disabled={ true }
+								value={ conversionBatchSize }
+							/>
+							<hr />
+							<h2>{ __( 'Reset ongoing conversion' ) }</h2>
+							<p>
+								{ __(
+									'In case that your active conversion browser tab has been closed by accident, or it has been interrupted and closed unexpectedly, you may reset the conversion status here, and start converting all over again.'
+								) }
+							</p>
+							<Notice noticeText={ __( 'This will enable you to restart the conversion, but any previous results may be lost.' ) } isWarning />
+							<Card noBackground className="newspack-card__buttons-card">
+								<Button
+									isPrimary
+									onClick={ this.handleOnClickResetConversion }
+								>
+									{ __( 'Reset conversion' ) }
+								</Button>
+							</Card>
+						</Card>
+					</Grid>
+				</Fragment>
 			);
 		} else {
 			return (
-				<div className="ncc-page">
-					<h1>{ __( 'Run Conversion' ) }</h1>
-
-					<p>
-						<b>
-							<u>{ __( 'Note carefully' ) }</u>
-						</b>
-						{ ' -- ' }
-						{ __(
-							'once started, the conversion should not be interrupted! Your browser page needs to remain active until conversion is complete. You may, however, manually open multiple browser tabs once the conversion starts, and each of them will fetch and convert another batch of posts in parallel. Running multiple tabs speeds up the conversion. Recommended max number of tabs is 4.'
-						) }
-					</p>
-
-					<h3>{ __( 'Planned Conversion' ) }</h3>
-					<ul>
-						<li>{ __( 'the entire queued content will be converted to Gutenberg Blocks' ) }</li>
-						<li>{ __( 'this page will automatically reload for every batch' ) }</li>
-						<li>
-							{ __( 'number of posts/entries to be converted:' ) }{' '}
-							<b>
-								<u>{ queuedEntries }</u>
-							</b>
-						</li>
-						<li>
-							{ __( 'total conversion batches queued' ) }:{' '}
-							<b>
-								<u>{ maxBatch }</u>
-							</b>
-						</li>
-						<li>
-							{ __( 'batch size (posts per batch):' ) }{' '}
-							<b>
-								<u>{ conversionBatchSize }</u>
-							</b>
-						</li>
-					</ul>
-
-					<br />
-
-					<input
-						type="submit"
-						className="large"
-						value={ __( 'Start Conversion' ) }
-						onClick={ this.handleOnClickInitializeConversion }
-					/>
-
-					<br />
-					<br />
-
-					{ !! someConversionsFailed && (
-						<div>
-							<h3>{ __( 'Retry Converting Failed Posts' ) }</h3>
-							<p>
-								{ __(
-									"Looks like some posts weren't converted properly. This could have happened due to unexpected/unsupported Post content (valid HTML source is expected as Post content), or due to technical reasons (e.g. your server experiencing difficulties). You may retry converting these posts now."
-								) }
-							</p>
-							<ul>
-								<li>
-									{ __( 'number of posts which failed getting converted:' ) }{' '}
-									<b>
-										<u>{ countFailedConverting }</u>
-									</b>
-								</li>
-							</ul>
-
-							<input
-								type="submit"
-								className="large"
-								value={ __( 'Retry Failed' ) }
-								onClick={ this.handleOnClickInitializeRetryFailed }
+				<Fragment>
+					<div className="newspack-logo-wrapper">
+						<NewspackLogo />
+					</div>
+					<Grid>
+						<FormattedHeader
+							headerIcon={ <UnarchiveIcon /> }
+							headerText={ __( 'Run conversion' ) }
+							subHeaderText={ __( 'The queued content will be converted to Gutenberg Blocks.' ) }
+						/>
+						<Card>
+							{ !! someConversionsFailed && (
+								<Fragment>
+									<Notice noticeText={ __( 'Looks like some entries weren\'t converted properly. You may retry converting these.' ) } isError />
+									<TextControl
+										label={ __( 'Number of failed entries' ) }
+										disabled={ true }
+										value={ countFailedConverting }
+									/>
+									<Card noBackground className="newspack-card__buttons-card">
+										<Button
+											isPrimary
+											onClick={ this.handleOnClickInitializeRetryFailed }
+										>
+											{ __( 'Retry conversion' ) }
+										</Button>
+									</Card>
+									<hr />
+								</Fragment>
+							) }
+							<Notice noticeText={ __( 'This page will automatically reload for every batch.' ) } isPrimary />
+							<TextControl
+								label={ __( 'Number of entries to be converted' ) }
+								disabled={ true }
+								value={ queuedEntries }
 							/>
-						</div>
-					) }
-				</div>
+							<TextControl
+								label={ __( 'Total conversion batches queued' ) }
+								disabled={ true }
+								value={ maxBatch }
+							/>
+							<TextControl
+								label={ __( 'Conversion batch size (entries per batch)' ) }
+								disabled={ true }
+								value={ conversionBatchSize }
+							/>
+							<Notice noticeText={ __( 'Once started, the conversion should not be interrupted! Your browser page needs to remain active until conversion is complete.' ) } isWarning />
+							<Card noBackground className="newspack-card__buttons-card">
+								<Button
+									isPrimary
+									onClick={ this.handleOnClickInitializeConversion }
+								>
+									{ __( 'Start conversion' ) }
+								</Button>
+							</Card>
+						</Card>
+					</Grid>
+				</Fragment>
 			);
 		}
 	}
