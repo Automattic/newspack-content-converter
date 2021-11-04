aac3aa5e6c26d04dc9c21f8d2a9bcfd2554b4229
Adam Borowski <adam@adamboro.com>
2021-11-04 16:40:21 +0100

    fix: editor interaction

--- assets/src/index.js
+++ assets/src/index.js
@@ -12,44 +12,6 @@ import Settings from './settings';
 import Conversion from './conversion';
 import './style.scss';
 
-const nccGetElementByClassName = function( className ) {
-	const elements = document.getElementsByClassName( className );
-	if ( 'undefined' == typeof elements || ! elements.length )
-		throw 'Not found element by class name ' + className;
-	return elements[ 0 ];
-};
-
-const nccHideElementByClass = function( className ) {
-	nccGetElementByClassName( className ).style.display = 'none';
-};
-
-const nccInsertRootAdjacentToElementByClass = function( className ) {
-	nccGetElementByClassName( className ).insertAdjacentHTML( 'afterend', '<div id="root"></div>' );
-};
-
-// Wrapper function which enables retrying a callback after a timeout interval and for a defined maxAttempts (useful to retry
-// actions for elements which haven't yet been injected into DOM).
-function nccCallbackWithRetry( callback, callbackParam, maxAttempts = 10, timeout = 1000 ) {
-	return new Promise( function( resolve, reject ) {
-		const doCallback = function( attempt ) {
-			try {
-				callback( callbackParam );
-				resolve();
-			} catch ( e ) {
-				if ( 0 == attempt ) {
-					console.log( 'Final CSS warning: ' + e );
-				} else {
-					setTimeout( function() {
-						doCallback( attempt - 1 );
-					}, timeout );
-					console.log( e );
-				}
-			}
-		};
-		doCallback( maxAttempts );
-	} );
-}
-
 window.onload = function() {
 	const div_settings = document.getElementById( 'ncc-settings' );
 	const div_conversion = document.getElementById( 'ncc-conversion' );
@@ -59,11 +21,9 @@ window.onload = function() {
 	} else if ( typeof div_conversion != 'undefined' && div_conversion != null ) {
 		render( <Conversion />, div_conversion );
 	} else {
-		// Converter app sits on top of the Gutenberg Block Editor.
-		nccCallbackWithRetry( nccHideElementByClass, 'edit-post-header' );
-		nccCallbackWithRetry( nccHideElementByClass, 'edit-post-layout__content' );
-		nccCallbackWithRetry( nccHideElementByClass, 'edit-post-sidebar' );
-		nccCallbackWithRetry( nccInsertRootAdjacentToElementByClass, 'edit-post-header' );
+		document
+			.querySelector( '.block-editor' )
+			.insertAdjacentHTML( 'afterend', '<div id="root"></div>' );
 
 		window.onbeforeunload = function() {};
 
